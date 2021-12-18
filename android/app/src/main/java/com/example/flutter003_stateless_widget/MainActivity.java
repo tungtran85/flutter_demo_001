a6cfe5e7dc91811fe225cbf86637f1e5beb62d22
tungtran85 <thanhtung12a32003@gmail.com>
2021-12-18 15:02:15 +0700

    Init

---
+++ android/app/src/main/java/com/example/flutter003_stateless_widget/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter003_stateless_widget;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp (
+   MaterialApp(
+     title: "My App",
+     home: MyScaffold(),
+   )
+  );
+}
+
+class MyScaffold extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text("Example Title",
+              style: Theme.of(context).primaryTextTheme.subtitle1,
+            ),
+            ),
+          Expanded(
+            child: Center(
+              child: Text('Hello word'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+
+  MyAppBar({required this.title});
+
+  final Widget title ;
+  @override
+  Widget build(BuildContext context)
+  {
+    //TODO: implement build
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: "Navigation menu",
+              onPressed: null,
+          ),
+          Expanded(
+           child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "Search",
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
